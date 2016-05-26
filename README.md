20c40cab9546a701a464251a73614163cf572916
MichelML <michmoreau.l@gmail.com>
2016-05-26 02:20:35 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+These scripts allows you to get the sum in dollars of all your orders you've made (per one page of orders) on either amazon.ca or amazon.com.
+
+## How to use it
+Choose the script applicable to the site you will visit (.ca or .com). Go on amazon.ca/com and make sure the site is in english. Then go on the page where you can view your orders. It should look something like: 
+ ```
+ https://www.amazon.ca/gp/your-account/order-history?...
+ ```
+Then open the developer console (you should use Chrome or Firefox for this), and copy paste the script directly into the console.  You should then see the sum of all the order prices of the page as a console output.
---
+++ amazon.com.js
@@ -0,0 +1,16 @@
+/**
+ * This script allows you to get the sum in dollars of all your orders 
+ * you've made (per one page of orders) on AMAZON.COM . To use the script,
+ * Go on amazon.ca and make sure the site is in english. Then go on the page where 
+ * you can view your orders (should look something like:
+ * https://www.amazon.ca/gp/your-account/order-history?...), open the developer console,
+ * (you should use Chrome or Firefox for this) and copy paste the script directly into the console. 
+ * You should then see the sum of all the order prices of the page as a console output.
+ */
+
+prices = document.getElementsByClassName('a-color-secondary value')
+var pricearr = []
+for (var price in prices) pricearr.push(prices[price]);
+newarr = pricearr.filter(function(value){ if (!value.innerHTML) {return false} else {return value.innerHTML.includes("$")}});
+numbers = newarr.map(function(value) {return parseFloat(value.innerHTML.replace( /^\D+/g, ''))});
+numbers.reduce(function(val,newval) {return val+newval},0);	 
